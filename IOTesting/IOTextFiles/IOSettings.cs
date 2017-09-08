52ba6890eb2e29a21e83df79d0d374bfb3656069
Hristo <hhristoff@yahoo.com>
2017-09-08 16:25:07 +0300

    Модул 08, 02.Създаване, запис, четене и проверка на пътища на файлове

---
+++ IOTesting/IOTextFiles/IOSettings.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOTextFiles
+{    
+    public class IOSettings
+    {
+        private STable _stable;
+
+        public IOSettings(STable stable)
+        {
+            _stable = stable;
+        }
+
+        public string GetPath()
+        {
+            // Програмата\Settings/settings.txt
+            string path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Settings");
+            path = System.IO.Path.Combine(path, "settings.txt"); // Но е добре самия файл да е извън дир-та н апрограмата!
+
+            // Други видиве директории:
+            //string user = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            //Console.WriteLine(user);
+            //string desctop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //Console.WriteLine(desctop);
+
+            return path;
+        }
+     
+
+        public bool Save() 
+        {
+            // когато се запеметява, винаги да го правим в TRY-CATCH!!!
+            try
+            {
+                string temp = "";
+                temp = String.Join("\r\n", _stable.stable);
+
+                //Запис на текстов файл:
+                System.IO.File.WriteAllText(GetPath(), temp);
+
+                return true;
+            }
+            catch {
+            }
+
+            return false;
+        }
+
+        public bool Open()
+        {
+            try
+            {
+                IniSettings();
+
+                string temp = "", filePath = GetPath();
+                    
+                if(System.IO.File.Exists(filePath))  // проверка дали пътят е валиден
+                {
+                    System.IO.File.ReadAllText(filePath);
+
+                    string[] table = temp.Replace("\r", "").Split('\n');
+
+                    for (int i = 0; i < table.Length; i++)
+                    {
+                        _stable.stable[i] = table[i];
+                    }
+                }
+                else
+                {                 
+                    return false;
+                }
+
+                System.Diagnostics.Process.Start(filePath);
+
+                return true;
+            }
+            catch {                
+            }
+            return false;
+        }
+
+        private void IniSettings()
+        {
+            try
+            {
+                bool fileExist = System.IO.File.Exists(GetPath());
+
+                if (!fileExist)
+                {
+                    string directory = System.IO.Path.GetDirectoryName(GetPath());
+
+                    // Застраховане, че директорията съществува
+                    if(!System.IO.Directory.Exists(directory))
+                    {
+                        System.IO.Directory.CreateDirectory(directory);
+                    }
+
+                    // Запаметяване съдържанието на файла
+                    Save();
+                }
+            }
+            catch 
+            {
+            }
+        }
+    }
+}
---
+++ IOTesting/IOTextFiles/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOTextFiles
+{
+    class Program
+    {
+      
+
+        static void Main()
+        {
+            STable sTable = new STable();
+            IOSettings io = new IOSettings(sTable);
+
+           
+
+            if(io.Open())                 //io.Save())
+            {
+                Console.WriteLine("Таблицата е прочетена успешно.");
+            }
+            else
+            {
+                Console.WriteLine("Таблицата НЕ е прочетена  успешно.");
+            }
+
+            Console.WriteLine(sTable.stable[2]);
+        }
+    }
+}
